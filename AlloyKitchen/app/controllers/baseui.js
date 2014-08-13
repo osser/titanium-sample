f416786ebc9ebb7ff82ea02487bc508ae77d42d0
Koma <admin@osser.jp>
2014-08-13 13:44:32 +0900

    updated

--- AlloyKitchen/app/controllers/baseui.js
+++ AlloyKitchen/app/controllers/baseui.js
@@ -1,6 +1,10 @@
 var args = arguments[0] || {};
 var lv_itemclick = function(e) {
 	var item = e.section.getItemAt(e.itemIndex);
-	console.log(item);
-	alert(item.properties.title);
+	if (item.properties.formid) {
+		var win = Alloy.createController('baseui/' + item.properties.formid).getView();
+		$.parentTab.open(win);
+	} else {
+		alert(item.properties.title);
+	}
 };
--- AlloyKitchen/app/controllers/index.js
+++ AlloyKitchen/app/controllers/index.js
@@ -1,1 +1,7 @@
 $.index.open();
+
+$.baseui.parentTab = $.tabBaseUI;
+$.controls.parentTab = $.tabControls;
+$.phone.parentTab = $.tabPhone;
+$.platform.parentTab = $.tabPlatform;
+$.mashups.parentTab = $.tabMashups;
