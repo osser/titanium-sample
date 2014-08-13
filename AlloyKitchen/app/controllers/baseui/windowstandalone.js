d69960ac1a90137a0a4545cbaee46e679871c779
Koma <admin@osser.jp>
2014-08-14 06:22:19 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/windowstandalone.js
+++ AlloyKitchen/app/controllers/baseui/windowstandalone.js
@@ -28,7 +28,44 @@ var btnOpenPlain_click = function(e) {
 };
 
 var btnTraditionalModel_click = function(e) {
+    var win = Ti.UI.createWindow({
+        title : 'Vibrate',
+        backgroundColor : '#336699',
+        layout : 'vertical'
+    });
+    win.addEventListener('close', function(e) {
+        win = null;
+    });
+    // Create a Button.
+    var aButton = Ti.UI.createButton({
+        top : 80,
+        width : '60%',
+        backgroundColor : '#eff4ff',
+        color : 'black',
+        title : '閉じる'
+    });
+    // Listen for click events.
+    aButton.addEventListener('click', function() {
+        win.close();
+    });
+    // Add to the parent view.
+    win.add(aButton);
 
+    // Create a Button.
+    var bButton = Ti.UI.createButton({
+        top : 40,
+        width : '60%',
+        backgroundColor : '#99b433',
+        title : 'Vibrate'
+    });
+    // Listen for click events.
+    bButton.addEventListener('click', function() {
+        Ti.Media.vibrate([0, 1000]);
+    });
+    // Add to the parent view.
+    win.add(bButton);
+
+    win.open();
 };
 
 var btnOpenAnimationFun_click = function(e) {
@@ -40,7 +77,26 @@ var btnOpenNavBarVisible_click = function(e) {
 };
 
 var btnOpenFullScreen_click = function(e) {
+    var win = Ti.UI.createWindow({
+        title : 'New Window (FullScreen)',
+        backgroundColor : '#369'
+    });
 
+    // Create a Button.
+    var aButton = Ti.UI.createButton({
+        title : 'Close',
+        color : 'white'
+    });
+    // Listen for click events.
+    aButton.addEventListener('click', function() {
+        win.close();
+    });
+    // Add to the parent view.
+    win.add(aButton);
+
+    win.open({
+        fullscreen : true
+    });
 };
 
 var btnOpenToolbar_click = function(e) {
