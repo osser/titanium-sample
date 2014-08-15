9e8ef5849c9621b2413693295e43a3c4d54ddad4
Koma <admin@osser.jp>
2014-08-16 04:37:16 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_scroll_style.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_scroll_style.js
@@ -1,1 +1,111 @@
 var args = arguments[0] || {};
+
+(function() {
+    $.listSection.setItems(getData());
+})();
+
+var listView_itemclick = function(e) {
+    switch(e.itemIndex ) {
+    case 0:
+        $.listView.backgroundColor = 'white';
+        $.listView.scrollIndicatorStyle = Ti.UI.iPhone.ScrollIndicatorStyle.DEFAULT;
+        break;
+    case 1:
+        $.listView.backgroundColor = 'white';
+        $.listView.scrollIndicatorStyle = Ti.UI.iPhone.ScrollIndicatorStyle.BLACK;
+        break;
+    case 2:
+        $.listView.backgroundColor = 'white';
+        $.listView.scrollIndicatorStyle = Ti.UI.iPhone.ScrollIndicatorStyle.WHITE;
+        break;
+    }
+};
+
+function getData() {
+    var data = [{
+        properties : {
+            title : 'STYLE DEFAULT'
+        }
+    }, {
+        properties : {
+            title : 'STYLE BLACK'
+        }
+    }, {
+        properties : {
+            title : 'STYLE WHITE'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }, {
+        properties : {
+            title : 'Item'
+        }
+    }];
+    return data;
+}
