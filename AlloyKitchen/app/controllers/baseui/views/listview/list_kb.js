083e27c0361b22cd2f1be225546ec793acf36fc9
Koma <admin@osser.jp>
2014-08-16 07:55:17 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_kb.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_kb.js
@@ -1,1 +1,18 @@
 var args = arguments[0] || {};
+
+(function() {
+    if (OS_IOS) {
+        $.frmWin.orientationModes = [Ti.UI.PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT];
+    }
+
+    var data = [];
+    for (var i = 0; i < 100; i++) {
+        data.push({
+            template : 'myCell',
+            bindField : {
+                value : 'Field ' + i
+            }
+        });
+    }
+    $.listSection.setItems(data);
+})();
