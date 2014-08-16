b23de798f23d3377cbcd8d2cc47a6207a024e4b6
Koma <admin@osser.jp>
2014-08-16 15:14:22 +0900

    updated

---
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_itemsappend.js
@@ -0,0 +1,16 @@
+var args = arguments[0] || {};
+
+var showSource = function(e) {
+    //alert('showSource');
+    var webshow = Alloy.createController('webshow', {
+        sourceUrl : 'baseui/views/listview/list_itemsappend',
+    }).getView();
+
+    if (OS_IOS) {
+        args.parentTab.open(webshow);
+    }
+    if (OS_ANDROID) {
+        webshow.open();
+    }
+};
+
