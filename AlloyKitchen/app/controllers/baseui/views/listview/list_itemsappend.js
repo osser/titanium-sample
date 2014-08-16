4e75c311f458b48151c0520c3086e1113ff62b51
Koma <admin@osser.jp>
2014-08-17 08:43:33 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_itemsappend.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_itemsappend.js
@@ -1,5 +1,62 @@
 var args = arguments[0] || {};
 
+function getData(thetitle) {
+    var data = [{
+        properties : {
+            title : 'Item - Animation ' + thetitle
+        }
+    }];
+    return data;
+}
+
+function getDataAndroid() {
+    var data = [];
+    for (var i = 0; i < 2; i++) {
+        data.push({
+            properties : {
+                title : 'Appended ' + i,
+                color : '#b91d47'
+            }
+        });
+    }
+    return data;
+}
+
+var interval = null;
+var i = 0;
+
+var btnStartTest_click = function(e) {
+
+    $.btnStartTest.enabled = false;
+    interval = setInterval(function() {
+        if (OS_IOS) {
+            var animationStyles = {
+                'None' : Ti.UI.iPhone.RowAnimationStyle.NONE,
+                'Left' : Ti.UI.iPhone.RowAnimationStyle.LEFT,
+                'Right' : Ti.UI.iPhone.RowAnimationStyle.RIGHT,
+                'Top' : Ti.UI.iPhone.RowAnimationStyle.TOP,
+                'Bottom' : Ti.UI.iPhone.RowAnimationStyle.BOTTOM,
+                'Fade' : Ti.UI.iPhone.RowAnimationStyle.FADE,
+            };
+            var animationsArray = ['None', 'Left', 'Right', 'Top', 'Bottom', 'Fade'];
+            var type = animationsArray[i];
+            $.listSection.appendItems(getData(type), {
+                animationStyle : animationStyles[type]
+            });
+            i = (i + 1) % animationsArray.length;
+        } else {
+            $.listSection.appendItems(getDataAndroid());
+        }
+    }, 1000);
+
+};
+
+var frm_close = function(e) {
+    if (interval !== null) {
+        clearInterval(interval);
+    }
+};
+
 var showSource = function(e) {
     //alert('showSource');
     var webshow = Alloy.createController('webshow', {
