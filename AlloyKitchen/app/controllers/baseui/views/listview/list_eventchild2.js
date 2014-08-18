692b7a95707598bc0b50b4a273a29596db6f6aac
Koma <admin@osser.jp>
2014-08-19 05:27:13 +0900

    updated

---
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_eventchild2.js
@@ -0,0 +1,15 @@
+var args = arguments[0] || {};
+
+var showSource = function(e) {
+    //alert('showSource');
+    var webshow = Alloy.createController('webshow', {
+        sourceUrl : 'baseui/views/listview/list_eventchild2',
+    }).getView();
+
+    if (OS_IOS) {
+        args.parentTab.open(webshow);
+    }
+    if (OS_ANDROID) {
+        webshow.open();
+    }
+};
