b50a16ec8d4a70e5be8df1717ccadd0019ba6d92
Koma <admin@osser.jp>
2014-08-20 06:41:36 +0900

    updated

---
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_v2_search_searchview.js
@@ -0,0 +1,19 @@
+var args = arguments[0] || {};
+
+(function() {
+
+})();
+
+var showSource = function(e) {
+    //alert('showSource');
+    var webshow = Alloy.createController('webshow', {
+        sourceUrl : 'baseui/views/listview/list_v2_search_searchview',
+    }).getView();
+
+    if (OS_IOS) {
+        args.parentTab.open(webshow);
+    }
+    if (OS_ANDROID) {
+        webshow.open();
+    }
+};
