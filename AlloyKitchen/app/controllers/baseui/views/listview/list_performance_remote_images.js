31f74a66a2745c2f10dbf52c8ab1266d79935ba6
Koma <admin@osser.jp>
2014-08-21 08:39:32 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_performance_remote_images.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_performance_remote_images.js
@@ -1,7 +1,8 @@
 var args = arguments[0] || {};
 
 (function() {
-
+    $.lblInfo.setText('This is a list View that uses a custom template that holds an imageView and a label.\n' + 'The imageView loads remote images. Thank you placehold.it\n' + 'Expected performance is a smooth scroll experience.\n' + 'On scrolling back and forth in the list view, the right image must be loaded.(since we are reusing views)\n\n');
+    $.section0.setItems(getData());
 })();
 
 var showSource = function(e) {
@@ -17,3 +18,29 @@ var showSource = function(e) {
         webshow.open();
     }
 };
+
+var btnShowMe_click = function(e) {
+    $.frmWin.remove($.lblInfo);
+    $.frmWin.remove($.btnShowMe);
+    $.listView.setVisible(true);
+};
+
+function getData() {
+    var data = [];
+    var baseUrl = 'http://placehold.it/';
+    var imageUrl;
+    for ( i = 30; i <= 60; i++) {
+        for ( j = 30; j <= 60; j++) {
+            imageUrl = baseUrl + i + 'x' + j;
+            data.push({
+                avatar : {
+                    image : imageUrl
+                },
+                info : {
+                    text : 'Loading Url\n' + imageUrl
+                }
+            });
+        }
+    }
+    return data;
+}
