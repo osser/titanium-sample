a0ce96fd8aeae06e849a59c9b16df2b7fcd4d364
Koma <admin@osser.jp>
2014-08-21 16:52:41 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_v2_index_bar.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_v2_index_bar.js
@@ -1,6 +1,49 @@
 var args = arguments[0] || {};
 
 (function() {
+	var fruitDataSet = [
+	    {properties: { title: 'Apple', searchableText:' Fruits Apple', itemId:'0 0'}},
+	    {properties: { title: 'Banana', searchableText:'Fruits Banana', itemId:'0 1'}},
+	    {properties: { title: 'Cantaloupe', searchableText:'Fruits Cantaloupe', itemId:'0 2'}},
+	    {properties: { title: 'Fig', searchableText:'Fruits Fig', itemId:'0 3'}},
+	    {properties: { title: 'Guava', searchableText:'Fruits Guava', itemId:'0 4'}},
+	    {properties: { title: 'Kiwi', searchableText:'Fruits Kiwi', itemId:'0 5'}},
+	];
+	$.fruitSection.setItems(fruitDataSet);
+
+	var vegDataSet = [
+	    {properties: { title: 'Carrots', searchableText:'Vegetables Carrots', itemId:'1 0'}},
+	    {properties: { title: 'Potatoes', searchableText:'Vegetables Potatoes', itemId:'1 1'}},
+	    {properties: { title: 'Corn', searchableText:'Vegetables Corn', itemId:'1 2'}},
+	    {properties: { title: 'Beans', searchableText:'Vegetables Beans', itemId:'1 3'}},
+	    {properties: { title: 'Tomato', searchableText:'Vegetables Tomato', itemId:'1 4'}},
+	];
+	$.vegSection.setItems(vegDataSet);
+
+	var fishDataSet = [
+	    {properties: { title: 'Cod', searchableText:'Fish Cod', itemId:'2 0'}},
+	    {properties: { title: 'Haddock', searchableText:'Fish Haddock', itemId:'2 1'}},
+	    {properties: { title: 'Salmon', searchableText:'Fish Salmon', itemId:'2 2'}},
+	    {properties: { title: 'Tuna', searchableText:'Fish Tuna', itemId:'2 3'}},
+	];
+	$.fishSection.setItems(fishDataSet);
+
+	var animalsDataSet = [
+	    {properties: { title: 'Deer', searchableText:'Animals Deer', itemId:'3 0'}},
+	    {properties: { title: 'Dog', searchableText:'Animals Dog', itemId:'3 1'}},
+	    {properties: { title: 'Cat', searchableText:'Animals Cat', itemId:'3 2'}},
+	    {properties: { title: 'Elephant', searchableText:'Animals Elephant', itemId:'3 3'}},
+	];
+	$.animalsSection.setItems(animalsDataSet);
+
+	var indices = [
+	{index:0,title:'Fru'},
+	{index:1,title:'Veg'},
+	{index:2,title:'Fis'},
+	{index:3,title:'Ani'}
+	];
+	
+	$.listView.sectionIndexTitles = indices;
 
 })();
 
