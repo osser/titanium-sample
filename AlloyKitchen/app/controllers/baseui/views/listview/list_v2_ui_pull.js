1907ca1959ed32c2feb158cace98f8fc2e81e814
Koma <admin@osser.jp>
2014-08-22 05:55:17 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_v2_ui_pull.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_v2_ui_pull.js
@@ -1,8 +1,33 @@
 var args = arguments[0] || {};
 
 (function() {
-    alert('Come soon...');
+    $.lblDesc.setText('This test shows the pullView support in ListView. The pullView property replaces the headerPullView property in TableView.\n\n' + 'In addition to the pullView property, the ListView also introducs two new events - pull and pullend.\n\n' + 'The pull event is fired whenever the user drags the listView past the top edge of the pullView.\n\n' + 'The pull event has a single custom argument active (boolean) which is true when the pullView is fully visible and false when it is partially visible\n\n' + 'The pullend event fires when the user stops dragging the ListView and the pullView is completely visible.\n\n');
+    
+	var fruitDataSet = [
+    	{properties: { title: 'Apple'}},
+    	{properties: { title: 'Banana'}},
+    	{properties: { title: 'Cantaloupe'}},
+    	{properties: { title: 'Fig'}},
+    	{properties: { title: 'Guava'}},
+    	{properties: { title: 'Kiwi'}},
+	];
+	$.fruitSection.setItems(fruitDataSet);
+	
+	    
 })();
+
+var btnUnderstand_click = function(e) {
+    $.frmWin.remove($.scrollView);
+};
+
+
+function getFormattedDate() {
+    var date = new Date();
+    return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+}
+
+var refreshCount = 0;
+
 
 var showSource = function(e) {
     //alert('showSource');
@@ -17,3 +42,71 @@ var showSource = function(e) {
         webshow.open();
     }
 };
+
+var listView_pull = function(e) {
+    console.log('listView_pull');
+    $.eventStatus.text = 'EVENT pull FIRED. e.active = ' + e.active;
+    if (e.active == false) {
+        var unrotate = Ti.UI.create2DMatrix();
+        $.imageArrow.animate({
+            transform : unrotate,
+            duration : 180
+        });
+        $.labelStatus.text = 'Pull down to refresh...';
+    } else {
+        var rotate = Ti.UI.create2DMatrix().rotate(180);
+        $.imageArrow.animate({
+            transform : rotate,
+            duration : 180
+        });
+        if (refreshCount == 0) {
+            $.labelStatus.text = 'Release to get Vegetables...';
+        } else {
+            $.labelStatus.text = 'Release to get Fish...';
+        }
+    }
+};
+
+
+var listView_pullend = function(e) {
+    console.log('listView_pullend');
+    $.eventStatus.text = 'EVENT pullend FIRED.';
+    if (refreshCount == 0) {
+        $.labelStatus.text = 'Loading Vegetables...';
+    } else {
+        $.labelStatus.text = 'Loading Fish...';
+    }
+    $.imageArrow.hide();
+    $.actInd.show();
+    $.listView.setContentInsets({
+        top : 80
+    }, {
+        animated : true
+    });
+    setTimeout(function() {
+    	alert('データロード処理');
+    	resetPullHeader();
+    }, 2000);
+
+};
+
+function resetPullHeader() {
+    $.actInd.hide();
+    $.imageArrow.transform = Ti.UI.create2DMatrix();
+    if (refreshCount < 2) {
+        $.imageArrow.show();
+        $.labelStatus.text = 'Pull down to refresh...';
+        $.labelLastUpdated.text = 'Last Updated: ' + getFormattedDate();
+    } else {
+        $.labelStatus.text = 'Nothing To Refresh';
+        $.labelLastUpdated.text = 'Go Cook Something';
+        $.listView.removeEventListener('pull', listView_pull);
+        $.listView.removeEventListener('pullend', listView_pullend);
+        $.eventStatus.text = 'Removed event listeners.';
+    }
+    $.listView.setContentInsets({
+        top : 0
+    }, {
+        animated : true
+    });
+}
--- AlloyKitchen/app/controllers/webshow.js
+++ AlloyKitchen/app/controllers/webshow.js
@@ -1,13 +1,34 @@
 var args = arguments[0] || {};
 
+var showurl = '';
+
 (function() {
     console.log('sourceUrl:', args.sourceUrl);
+    showurl = args.sourceUrl;
+    btnShowController_click(null);
     //https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/views/baseui/tabgroups.xml
-    //$.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/views/' + args.sourceUrl + '.xml';
-    $.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/controllers/' + args.sourceUrl + '.js';
+    //$.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/controllers/' + args.sourceUrl + '.js';
 })();
 
 var frm_close = function(e) {
     $.webView = null;
     $.webshow = null;
+};
+
+var showToolBar = function(e) {
+    console.log('1', $.toolBar.visible, !$.toolBar.visible);
+    $.toolBar.visible = !$.toolBar.visible;
+    console.log('2', $.toolBar.visible, !$.toolBar.visible);
+};
+
+function btnShowStyle_click(e) {
+    $.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/styles/' + args.sourceUrl + '.tss';
+};
+
+function btnShowController_click(e) {
+    $.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/controllers/' + args.sourceUrl + '.js';
+};
+
+function btnShowView_click(e) {
+    $.webView.url = 'https://github.com/osser/titanium-sample/blob/master/AlloyKitchen/app/views/' + args.sourceUrl + '.xml';
 };
