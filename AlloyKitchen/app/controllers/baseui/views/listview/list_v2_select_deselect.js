c2526a21cdc9e5cbffb3b336693d4c369ba7cc9d
Koma <admin@osser.jp>
2014-08-21 13:34:09 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/listview/list_v2_select_deselect.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_v2_select_deselect.js
@@ -1,8 +1,32 @@
 var args = arguments[0] || {};
 
 (function() {
-
+    $.listView.sections = genSections();
 })();
+
+function genData(section) {
+    var data = [];
+    for ( i = 0; i < 10; i++) {
+        data.push({
+            properties : {
+                title : 'Section ' + section + ' Item ' + i
+            }
+        });
+    }
+    return data;
+}
+
+function genSections() {
+    var sections = [];
+    for ( j = 0; j < 10; j++) {
+        var section = Ti.UI.createListSection({
+            headerTitle : 'Section ' + j
+        });
+        section.setItems(genData(j));
+        sections.push(section);
+    }
+    return sections;
+}
 
 var showSource = function(e) {
     //alert('showSource');
@@ -16,4 +40,25 @@ var showSource = function(e) {
     if (OS_ANDROID) {
         webshow.open();
     }
+};
+
+var sectionIndex;
+var itemIndex;
+
+var btnB1_click = function(e) {
+    sectionIndex = Math.floor(Math.random() * 173) % 10;
+    itemIndex = Math.floor(Math.random() * 83) % 10;
+    $.listView.selectItem(sectionIndex, itemIndex);
+    $.btnB2.enabled = true;
+};
+
+var btnB2_click = function(e) {
+    $.listView.deselectItem(sectionIndex, itemIndex);
+    $.btnB2.enabled = false;
+};
+
+var listView_click = function(e) {
+    sectionIndex = e.sectionIndex;
+    itemIndex = e.itemIndex;
+    $.btnB2.enabled = true;
 };
--- AlloyKitchen/app/controllers/baseui/views/listview/list_v2_ui_misc.js
+++ AlloyKitchen/app/controllers/baseui/views/listview/list_v2_ui_misc.js
@@ -1,8 +1,34 @@
 var args = arguments[0] || {};
 
 (function() {
-
+    $.listView.sections = genSections();
 })();
+
+function genData(section) {
+    var data = [];
+    for ( i = 0; i < 5; i++) {
+        data.push({
+            properties : {
+                title : 'Section ' + section + ' Item ' + i,
+                backgroundColor : 'white',
+                color : 'blue'
+            }
+        });
+    }
+    return data;
+}
+
+function genSections() {
+    var sections = [];
+    for ( j = 0; j < 5; j++) {
+        var section = Ti.UI.createListSection({
+            headerTitle : 'Section ' + j
+        });
+        section.setItems(genData(j));
+        sections.push(section);
+    }
+    return sections;
+}
 
 var showSource = function(e) {
     //alert('showSource');
@@ -16,4 +42,36 @@ var showSource = function(e) {
     if (OS_ANDROID) {
         webshow.open();
     }
+};
+
+var canScroll = true;
+var zeroInsets = true;
+var animate = true;
+
+var btnCanScroll_click = function(e) {
+    canScroll = !canScroll;
+    $.listView.canScroll = canScroll;
+};
+
+var btnInsets_click = function(e) {
+    if (zeroInsets == true) {
+        $.listView.setContentInsets({
+            top : 20,
+            bottom : 20
+        }, {
+            animated : animate
+        });
+    } else {
+        $.listView.setContentInsets({
+            top : 0,
+            left : 0,
+            right : 0,
+            bottom : 0
+        }, {
+            animated : animate
+        });
+        animate = !animate;
+    }
+    zeroInsets = !zeroInsets;
+
 };
