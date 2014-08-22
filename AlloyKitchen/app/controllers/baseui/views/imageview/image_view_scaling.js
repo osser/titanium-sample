55531c419ba39547fdf852cd407ed1685a959766
Koma <admin@osser.jp>
2014-08-23 06:01:27 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/imageview/image_view_scaling.js
+++ AlloyKitchen/app/controllers/baseui/views/imageview/image_view_scaling.js
@@ -17,3 +17,14 @@ var showSource = function(e) {
         webshow.open();
     }
 };
+
+var lblInfo_click = function(e) {
+    $.lblInfo.text = 'retrieving dimensions...';
+    var blob = $.imageView.toBlob();
+    if (blob === null) {
+        $.lblInfo.text = 'Unable to retrieve image dimensions. The image is a remote url -- are you connected to the network? Or the ' + TIMEOUT_SECS + ' second timeout expired. Tap here to try again.';
+    } else {
+        $.lblInfo.text = blob.width + "x" + blob.height;
+        $.lblInfo.removeEventListener('click', lblInfo_click);
+    }
+};
