2f3c0355b00700682291c08e03a4fc0e86221dc3
Koma <admin@osser.jp>
2014-08-23 04:40:59 +0900

    imageview

---
+++ AlloyKitchen/app/controllers/baseui/views/imageview/image_view_file.js
@@ -0,0 +1,19 @@
+var args = arguments[0] || {};
+
+(function() {
+
+})();
+
+var showSource = function(e) {
+    //alert('showSource');
+    var webshow = Alloy.createController('webshow', {
+        sourceUrl : 'baseui/views/imageview/image_view_file',
+    }).getView();
+
+    if (OS_IOS) {
+        args.parentTab.open(webshow);
+    }
+    if (OS_ANDROID) {
+        webshow.open();
+    }
+};
