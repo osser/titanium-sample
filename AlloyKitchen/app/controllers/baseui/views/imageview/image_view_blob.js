978fb859c0e6a75b8b0b6a4a8cda89a63695bf57
Koma <admin@osser.jp>
2014-08-23 13:47:23 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/imageview/image_view_blob.js
+++ AlloyKitchen/app/controllers/baseui/views/imageview/image_view_blob.js
@@ -1,7 +1,17 @@
 var args = arguments[0] || {};
 
 (function() {
+    console.log(args.parentTab);
+    
+    // start a blob async and notify callback when completed
+    args.parentTab.toImage(function(e) {
+        Ti.API.info("tiGroup blob has been rendered: " + e.blob.width + "x" + e.blob.height);
+    });
 
+	var blob = args.parentTab.toImage();
+	console.log(blob);
+	
+    $.imageView.image = args.parentTab.toImage();
 })();
 
 var showSource = function(e) {
