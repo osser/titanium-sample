149a64a4e3d4812eb796bc12ec57c845b7e2aedf
Koma <admin@osser.jp>
2014-08-24 10:13:26 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/scrollview/scroll_views_scrollable.js
+++ AlloyKitchen/app/controllers/baseui/views/scrollview/scroll_views_scrollable.js
@@ -1,8 +1,32 @@
 var args = arguments[0] || {};
 
 (function() {
+    // orientation change listener
+    Ti.Gesture.addEventListener('orientationchange', function(e) {
+        // get orienation from event object
+        var orientation = getOrientation(e.orientation);
+    });
 
 })();
+
+function getOrientation(o) {//Came from orientation.js, but we didn't need the buttons and such
+    switch (o) {
+    case Titanium.UI.PORTRAIT:
+        return 'portrait';
+    case Titanium.UI.UPSIDE_PORTRAIT:
+        return 'upside portrait';
+    case Titanium.UI.LANDSCAPE_LEFT:
+        return 'landscape left';
+    case Titanium.UI.LANDSCAPE_RIGHT:
+        return 'landscape right';
+    case Titanium.UI.FACE_UP:
+        return 'face up';
+    case Titanium.UI.FACE_DOWN:
+        return 'face down';
+    case Titanium.UI.UNKNOWN:
+        return 'unknown';
+    }
+}
 
 var showSource = function(e) {
     //alert('showSource');
