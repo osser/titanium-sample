74f4bd2c057fd3492b2bdce16b798d57438e0b08
Koma <admin@osser.jp>
2014-08-24 12:28:24 +0900

    updated

--- AlloyKitchen/app/controllers/baseui/views/scrollview/scroll_views_dragging.js
+++ AlloyKitchen/app/controllers/baseui/views/scrollview/scroll_views_dragging.js
@@ -17,3 +17,24 @@ var showSource = function(e) {
         webshow.open();
     }
 };
+
+var scrollView_dragstart = function(e) {
+    Ti.API.info('Drag started');
+    $.message1.text = "Drag Started!";
+    $.message1.show();
+    setTimeout(function() {
+        $.message1.hide();
+    }, 1000);
+
+};
+
+var scrollView_dragend = function(e) {
+    Ti.API.info(e.type + ' detected! Decelerating? ' + (e.decelerate ? 'Yes' : 'No'));
+    $.message2.text = e.type + " detected! Decelerating :" + (e.decelerate ? 'Yes' : 'No');
+    $.message2.show();
+    setTimeout(function() {
+        $.message2.hide();
+    }, 1000);
+
+};
+
